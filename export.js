d0cb97e3db30bbc18d1a183a9efd1ffa0d22c04c
Mark Stier <stier020@Marks-MacBook-Pro-2.local>
2017-04-30 12:14:09 -0500

    fix parser import

--- export.js
+++ export.js
@@ -4,17 +4,17 @@ module.exports = {
   DIV_REPORTER:require("./parsers/process-div-reporter").parse,
   HTML_AST: require("./parsers/process-html-ast").parse,
   GENERIC: require("./parsers/process-generic-actions").parse,
-  VALIDATOR: require("./parsers/process-validator").parser,
+  VALIDATOR: require("./parsers/process-validator").parse,
   TOLERANT_GENERIC: require("./parsers/process-tolerant-generic-actions").parse,
-  TOLERANT_VALIDATOR: require("./parsers/process-tolerant-validator").parser,
-  TOLERANT_HTML_AST: require("./parsers/process-tolerant-html-ast").parser,
+  TOLERANT_VALIDATOR: require("./parsers/process-tolerant-validator").parse,
+  TOLERANT_HTML_AST: require("./parsers/process-tolerant-html-ast").parse,
   RAW:{
     DIV_REPORTER:require("./parsers/div-reporter").parse,
     HTML_AST: require("./parsers/html-ast").parse,
     GENERIC: require("./parsers/generic-actions").parse,
-    VALIDATOR: require("./parsers/validator").parser,
+    VALIDATOR: require("./parsers/validator").parse,
     TOLERANT_GENERIC: require("./parsers/tolerant-generic-actions").parse,
-    TOLERANT_VALIDATOR: require("./parsers/tolerant-validator").parser,
-    TOLERANT_HTML_AST: require("./parsers/tolerant-html-ast").parser,
+    TOLERANT_VALIDATOR: require("./parsers/tolerant-validator").parse,
+    TOLERANT_HTML_AST: require("./parsers/tolerant-html-ast").parse,
   }
 }
